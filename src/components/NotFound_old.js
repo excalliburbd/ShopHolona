fa877eff0c9da32a42785e49b8217ce927e4b74d
smazad1212 <smazad1212@gmail.com>
2017-06-21 15:06:07 +0600

    not found page added

---
+++ src/components/NotFound_old.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './NotFound.css';
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <div>404</div>
+      <div>Page Not Found</div>
+    </div>
+  );
+}
+
+// export default NotFound;
