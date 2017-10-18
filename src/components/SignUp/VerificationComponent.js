9e0622d7caf79010822ab80d8ec0b06574f1e017
Mohaiminislam94 <mohaiminislam94@gmail.com>
2017-10-18 17:22:45 +0600

    TG-122 style update

--- src/components/SignUp/VerificationComponent.js
+++ src/components/SignUp/VerificationComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Input from 'react-toolbox/lib/input/Input';
 import Button from 'react-toolbox/lib/button/Button';
+import './VerificationComponent.css';
 
 const VerificationComponent = ({
   verification,
@@ -22,7 +23,7 @@ const VerificationComponent = ({
               className='verification-login-btn sh-btn--yellow'
               onClick={ handleVerify } />
       <Button label='Resend Verification Code'
-              className='verification-resend-btn sh-btn--yellow'
+              className='verification-resend-btn'
               onClick={ handleVerify } />
     </div>
   )
