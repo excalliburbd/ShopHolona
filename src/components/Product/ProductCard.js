fa02b9c9a18704d6fdf59988e9b14030e3826ce0
Sk. Imtiaz Ahmed <excalliburbd@gmail.com>
2017-12-19 18:21:51 +0600

    no errors

--- src/components/Product/ProductCard.js
+++ src/components/Product/ProductCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import classnames from 'classnames';
 
 import Card from 'react-toolbox/lib/card/Card';
 import CardMedia from 'react-toolbox/lib/card/CardMedia';
